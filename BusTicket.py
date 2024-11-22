46c13de000b914cf57757b572c9bf1165ec8a56e
prince <p.mbonyumug@alustudent.com>
2024-11-22 07:30:07 +0200

    prince commit

--- BusTicket.py
+++ BusTicket.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 from datetime import datetime
-
+#this is the class of the User
 class User:
     def __init__(self, name, email, telephone, location):
         self.name = name
         self.email = email
         self.telephone = telephone
         self.location = location
-    
+
+
+    #this is the function that get the user information
     def get_user_info(self):
         return f"Name: {self.name}, Email: {self.email}, Phone: {self.telephone}, Location: {self.location}"
 
+#this is the class of admin that contain the data of the admin
 class Admin:
     def __init__(self):
         self.username = None
         self.password = None
 
+#this is the funtion that set the credentials of the admin
     def set_credentials(self):
         """Set up admin username and password for the first time."""
         print("Set up your admin account:")
